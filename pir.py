90bd5e89b5fd1976ceb52f920a71a3de1a012cb9
george-iot <georgeshenoda@protonmail.com>
2025-10-06 19:09:51 +0300

    pir

--- pir.py
+++ pir.py
@@ -1,3 +1,9 @@
 from gpiozero import MotionSensor
 
-pir = MotionSensor()
+pir = MotionSensor(17)
+
+if __name__ == "__main__":
+    pir.wait_for_motion()
+    print("Motion detected!")
+    pir.wait_for_no_motion()
+    print("Motion ended!")
