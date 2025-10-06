c9658d4a897e89731c53a06d080a76d00c570b51
george-iot <georgeshenoda@protonmail.com>
2025-10-06 19:56:58 +0300

    buzzer

--- buzzer.py
+++ buzzer.py
@@ -10,7 +10,8 @@ def buzz_off():
 
 if __name__ == "__main__":
     while True:
-        if time.time().hour < 18:
+        x = int(input())
+        if time.time().hour < x:
             continue
         buzz_on()
         print("Buzzer is ON")
