238dcc1374c1c3d36fbcccf6ae65f9ec2ee9e888
george-iot <georgeshenoda@protonmail.com>
2025-10-08 11:57:16 +0300

    faceRecognition

--- facerecognition.py
+++ facerecognition.py
@@ -1,18 +1,18 @@
 from paho.mqtt.client import Client
 
-client = Client(client_id="raspberryPi")
-client.connect("localhost", 1883, 60)
-client.subscribe("office/face_recognition")
-client.loop_forever()
-
-recognized = False
-
-def on_message(message):
+def on_message(client, userdata, message):
     global recognized
-    print(f"Received message: {message.payload.decode('utf-8')}")
     if str(message.payload.decode("utf-8")) == "1":
         recognized = True
     else:
         recognized = False
 
+
+client = Client(client_id="raspberryPi")
+client.connect("localhost", 1883)
 client.on_message = on_message
+client.subscribe("office/face_recognition")
+client.loop_forever()
+
+recognized = False
+
