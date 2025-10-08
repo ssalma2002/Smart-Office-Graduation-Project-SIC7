2096aa678db886a64dcdc803409a9fd6549bce8d
Salma Youssef <syoussef4444@gmail.com>
2025-10-08 15:31:02 +0100

    button

--- button.py
+++ button.py
@@ -4,5 +4,6 @@ mainDoor = Button(2)
 #adminDoors = Button()
 
 if __name__ == "__main__":
-    mainDoor.on_press = lambda: print("Main Door Button Pressed")
+    while True:
+        mainDoor.when_pressed = lambda: print("Main Door Button Pressed")
     #adminDoors.on_press = lambda: print("Admin Door Button Pressed")
